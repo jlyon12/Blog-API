08a0124be374e84bdcdfdfbb6605e45315e5e4ba
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-13 15:03:25 -0600

    create login static method on user model

--- models/userModel.js
+++ models/userModel.js
@@ -62,5 +62,24 @@ UserSchema.statics.signup = async function (
 
 	return user;
 };
+UserSchema.statics.login = async function (email, password) {
+	if (!email || !password) {
+		throw Error('All fields are required');
+	}
+
+	const user = await this.findOne({ email });
+
+	if (!user) {
+		throw Error('Incorrect email');
+	}
+
+	const match = await bcrypt.compare(password, user.password);
+
+	if (!match) {
+		throw Error('Incorrect password');
+	}
+
+	return user;
+};
 
 module.exports = mongoose.model('User', UserSchema);
