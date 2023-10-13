f0b2977e22391b7acd9b2a0f0cff4b1de577b155
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-13 14:57:38 -0600

    create signup static method on user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,7 @@
+/* eslint-disable camelcase */
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -6,13 +9,58 @@ const UserSchema = new Schema(
 	{
 		first_name: { type: String, required: true },
 		last_name: { type: String, required: true },
-		username: { type: String, required: true },
-		email: { type: String, required: true },
+		username: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
 		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 	},
 	{ timestamps: true }
 );
+
+UserSchema.statics.signup = async function (
+	first_name,
+	last_name,
+	username,
+	email,
+	password
+) {
+	const emailExists = await this.findOne({ email });
+	const usernameExists = await this.findOne({ username });
+
+	if (!first_name || !last_name || !username || !email || !password) {
+		throw Error('All fields are required');
+	}
+
+	if (!validator.isEmail(email)) {
+		throw Error('Email must be a valid email');
+	}
+
+	if (!validator.isStrongPassword(password)) {
+		throw Error(
+			'Password must be at least 8 characters long and include at least one lowercase, uppercase, number and symbol'
+		);
+	}
+
+	if (emailExists) {
+		throw Error('This email is already registered');
+	}
+
+	if (usernameExists) {
+		throw Error('This username is taken');
+	}
+
+	const salt = await bcrypt.genSalt(15);
+	const hash = await bcrypt.hash(password, salt);
+	const user = await this.create({
+		first_name,
+		last_name,
+		username,
+		email,
+		password: hash,
+	});
+
+	return user;
+};
 
 module.exports = mongoose.model('User', UserSchema);
