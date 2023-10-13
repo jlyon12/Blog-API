5920c45f06b05738c34748a5efe2e3f6bbaaac44
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-13 10:34:41 -0600

    define comment schema

---
+++ models/commentModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema(
+	{
+		body: { type: String, required: true },
+		author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model('Comment', CommentSchema);
