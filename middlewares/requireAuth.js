a43817d9ffb57b4701fe6953124b978b37e35a07
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-13 15:16:43 -0600

    create auth middleware

---
+++ middlewares/requireAuth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const requireAuth = async (req, res, next) => {
+	const { authorization } = req.headers;
+
+	if (!authorization) {
+		res.status(401).json({ err: 'Authorization Required' });
+		return;
+	}
+
+	const token = authorization.split(' ')[1];
+
+	try {
+		const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = await User.findById(_id).select('_id');
+		next();
+	} catch (err) {
+		res.status(401).json({ err: 'Request not authorized' });
+	}
+};
+
+module.exports = requireAuth;
