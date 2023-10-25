4b2e0fd66e9a30ca80b13f766e2439f80e3acbbe
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-24 19:55:34 -0600

    fix naming conventions on routes

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -7,7 +7,7 @@ const commentController = require('../controllers/commentController');
 // Get all comments
 router.get('/', commentController.get_post_comments);
 // Get single comment
-router.get('/:comment_id', commentController.get_single_post_comment);
+router.get('/:id', commentController.get_single_post_comment);
 
 // Protect routes below
 router.use(requireAuth);
@@ -15,6 +15,6 @@ router.use(requireAuth);
 // Create a new comment
 router.post('/', commentController.create_comment);
 // Delete a comment
-router.delete('/:comment_id', commentController.delete_comment);
+router.delete('/:id', commentController.delete_comment);
 
 module.exports = router;
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -7,7 +7,7 @@ const postController = require('../controllers/postController');
 // Get public posts
 router.get('/published', postController.get_published_posts);
 // Get single post
-router.get('/:post_id', postController.get_post);
+router.get('/:id', postController.get_post);
 
 // Protect routes below
 router.use(requireAuth);
@@ -17,8 +17,8 @@ router.get('/', postController.get_all_posts);
 // Create a new post
 router.post('/', postController.create_post);
 // Delete a post
-router.delete('/:post_id', postController.delete_post);
+router.delete('/:id', postController.delete_post);
 // Update a post
-router.patch('/:post_id', postController.update_post);
+router.patch('/:id', postController.update_post);
 
 module.exports = router;
